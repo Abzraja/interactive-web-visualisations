73aa2e4f713d12f58e87374266cd8d830c904eef
Abdurrahman Raja <abdurahman_raja@hotmail.com>
2021-10-03 13:39:54 +0100

    metadata complete

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -1,6 +1,9 @@
 // Data File Path
 const data_file = "../code/data/samples.json";
 
+function optionChanged() {
+
+
 // Fetch the JSON data and console log it
   main_data = d3.json(data_file).then(function(data) {
     console.log(data);
@@ -15,12 +18,32 @@ const data_file = "../code/data/samples.json";
       d3.select("select").append("option").attr("value", i).text(samples[i]["id"]);
     };
 
-    x = data.samples[0].sample_values.slice(0,10).reverse();
-    y = data.samples[0].otu_ids.slice(0,10).reverse();
-    t = data.samples[0].otu_labels;
-   // for (i in y) {y[i] = "OTU "+y[i]};
+  
+    //let dataset_index = 0;
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset"); 
+
+    // Assign the value of the dropdown menu option to a variable
+    var dataset_index = dropdownMenu.property("value");
+    console.log(dataset_index) 
+    x = data.samples[dataset_index].sample_values.slice(0,10).reverse();
+    y = data.samples[dataset_index].otu_ids.slice(0,10).reverse();
+    t = data.samples[dataset_index].otu_labels;
+    for (i in y) {y[i] = "OTU "+y[i]};
     console.log(x)
     console.log(y)
+
+  
+  //individual's metadata object
+  metadata = data.metadata[dataset_index]
+  meta_keys = Object.keys(metadata)
+  meta_values = Object.values(metadata)
+
+  console.log(meta_keys)
+  console.log(meta_values)
+  
+  d3.select("#sample-metadata").html("")
+  for (i in meta_keys) {d3.select("#sample-metadata").append("p").append("span").text(`${meta_keys[i]}: ${meta_values[i]}`)}
 
     // Display the default plot
     var bar_trace = {
@@ -32,15 +55,16 @@ const data_file = "../code/data/samples.json";
       orientation:"h"
     };
 
+
     var bubble_trace = {
-      x: y,
-      y: x,
+      y: data.samples[dataset_index].sample_values,
+      x: data.samples[dataset_index].otu_ids,
       mode: "markers",
       marker:{
-      size: x,
-      color: y
+        size: data.samples[dataset_index].sample_values,
+        color: data.samples[dataset_index].otu_ids,
       },
-      text: t,
+      text: data.samples[dataset_index].otu_labels,
       };
 
     let bar_data = [bar_trace]
@@ -52,15 +76,20 @@ const data_file = "../code/data/samples.json";
 
   })
 
-  function optionChanged() {
-    
-    // Use D3 to select the dropdown menu
-    var dropdownMenu = d3.select("#selDataset"); 
-  
-    // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
-    console.log(dataset)
+
+
+}
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  optionChanged()
+});
+
+
 
   
 
-  }
+  
